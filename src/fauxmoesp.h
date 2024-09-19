a83b74c1d725533b2d72f0e0135ff7d6fb1e85fb
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-09-19 23:18:24 +0200

    Update fauxmoesp.h

--- src/fauxmoesp.h
+++ src/fauxmoesp.h
@@ -54,8 +54,8 @@ THE SOFTWARE.
 #ifndef DEBUG_FAUXMO_VERBOSE_UDP
 #define DEBUG_FAUXMO_VERBOSE_UDP    false
 #endif
-
-#include <Arduino.h>
+#include "esphome.h"
+//#include <Arduino.h>
 
 #if defined(ESP8266)
     #include <ESP8266WiFi.h>
