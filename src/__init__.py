28a034a73bb25853b1833f2369c12af20e0572f0
creepystefan <147045262+creepystefan@users.noreply.github.com>
2024-10-25 15:05:18 +0200

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,24 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+myalexa_ns = cg.esphome_ns.namespace("realdash")
+MYALEXA = myalexa_ns.class_("MYALEXA", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(MYALEXA),
+                       
+    }
+).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    
+    
+
+
+    
