b3cb73a0cab13f4dcb51c9a15ce716f0245d8ac1
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-16 21:02:25 +0100

    Update __init__.py

--- components/myalexa/__init__.py
+++ components/myalexa/__init__.py
@@ -13,6 +13,7 @@ from esphome.const import CONF_ID
 cg.add_library('subtixx/FauxmoESP', '2.0.0')
 
 AUTO_LOAD = ["udp"]
+AUTO_LOAD = ["Async_tcp"]
 
 myalexa_ns = cg.esphome_ns.namespace("myalexa")
 MYALEXA = myalexa_ns.class_("MYALEXA", cg.Component)
