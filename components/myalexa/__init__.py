c686a9b4e31b8b7d9691fc2ab0b37f455f2e68ce
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-16 21:46:06 +0100

    Update __init__.py

--- components/myalexa/__init__.py
+++ components/myalexa/__init__.py
@@ -30,7 +30,7 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await cg.udp_component()
+    
     
     
     
