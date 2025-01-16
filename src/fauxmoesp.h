edb779bcf91bc0f60b2e3419584315aa211df5fd
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-16 22:11:36 +0100

    Update fauxmoesp.h

--- src/fauxmoesp.h
+++ src/fauxmoesp.h
@@ -56,7 +56,7 @@ THE SOFTWARE.
 #endif
 
 #include <Arduino.h>
-#include "esphome\components\udp\udp_component.h"
+//#include "esphome\components\udp\udp_component.h"
 //#include <AsyncTCP.h>
 //#include <AsyncEventSource.h>
 //#include <ESPAsyncTCP.h>
