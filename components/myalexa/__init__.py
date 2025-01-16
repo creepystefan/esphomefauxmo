1d4b6ef7d4bebe6ed7f778dc5e5eefcf876d3569
creepystefan <147045262+creepystefan@users.noreply.github.com>
2025-01-16 21:44:18 +0100

    Update __init__.py

--- components/myalexa/__init__.py
+++ components/myalexa/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+await cg.udp_component()
 #cg.add_library(
 #    name="ESPFauxmo",
 #    repository="https://github.com/creepystefan/esphomefauxmo",
